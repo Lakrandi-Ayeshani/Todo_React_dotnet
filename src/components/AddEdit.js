eddeedbf2478ba5a0017fa3408d2afa079528067
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2024-05-16 15:17:42 +0530

    README updated and styles changed

--- src/components/AddEdit.js
+++ src/components/AddEdit.js
@@ -3,29 +3,36 @@ import React from 'react';
 
 const AddEdit = (props) => {
 
-  const addNewTodo = async (todo) => {
-    if(todo) {
-      try {
-        const response = await axios.post('https://localhost:7068/api/TodoItems', 
-          {
-            "name": todo,
-            "isComplete": false
-          }
-        )
-        
-      } catch (err) {
-        console.error(err);
-      }
+  const handleButtonClick = () => {
+    if(props.edit) {
+      console.log("1")
+      editTodo();
+    } else {
+      addNewTodo();
+    }
+  }
+
+  const addNewTodo = async () => {
+    try {
+      const response = await axios.post('https://localhost:7068/api/TodoItems', 
+        {
+          "name": props.todo,
+          "isComplete": false
+        }
+      )
+      
+    } catch (err) {
+      console.error(err);
     }
   };
 
-  const editTodo = async (todo, id) => {
-    console.log("he")
-    console.log(todo, id)
+  const editTodo = async () => {
     try {
-      const response = await axios.put(`https://localhost:7068/api/TodoItems/${id}`, 
+      const response = await axios.put(`https://localhost:7068/api/TodoItems/${props.edit.id}`, 
         {
-          "name": todo,
+          "id": props.edit.id,
+          "name": props.todo,
+          "isComplete": props.edit.isComplete
         }
       )
       props.setTodo(null);
@@ -38,13 +45,14 @@ const AddEdit = (props) => {
   return (
     <div className='bg-gray-100 p-2 rounded-lg font-semibold w-[90%] flex justify-between'>
         <input
-          className='bg-gray-100 rounded-lg me-5 w-[90%] p-1'
+          placeholder='Add todo...'
+          className='input bg-gray-100 rounded-lg me-5 w-[90%] p-1 placeholder:italic placeholder:font-thin '
           value={props.todo} 
           onChange={e => props.setTodo(e.target.value)}
         /> 
         <button 
           className='add-button '
-          onClick={props.edit ? () => editTodo(props.todo, props.id) : () => addNewTodo(props.todo)}
+          onClick={handleButtonClick}
         >
             {props.edit ? "Edit" : "Add"}
         </button>
